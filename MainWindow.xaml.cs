80dfe5060850d9b73e686c47ed9a26e73217dc88
CPLjynxedCreations <CPLjynxedCreations@outlook.com>
2024-08-04 20:26:21 +1000

    finished basic item adding with edit button to be moved to till system

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,26 +19,43 @@ namespace ButtenTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool boolIsEdit { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
-
+            this.DataContext = this;
             button1.Style = (Style)Application.Current.Resources["Style2"];
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            Window2 prompt = new Window2();
-            prompt.Owner = Application.Current.MainWindow;
-            prompt.WindowStartupLocation = WindowStartupLocation.CenterOwner;
-            prompt.SizeToContent = SizeToContent.WidthAndHeight;
-            //prompt.Width = 1500;
-            //prompt.Height = 500;
-            prompt.button2.Click += (sender, e) => { prompt.Close(); };
-            prompt.button3.Click += (sender, e) => { prompt.Close(); };
-            prompt.ShowDialog();
-            button1.Content = prompt.name;
-            button1.Style = (Style)Application.Current.Resources[prompt.style];
+            if (boolIsEdit)
+            {
+                Window2 prompt = new Window2();
+                prompt.Owner = Application.Current.MainWindow;
+                prompt.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                prompt.SizeToContent = SizeToContent.WidthAndHeight;
+                //prompt.Width = 1500;
+                //prompt.Height = 500;
+                prompt.button2.Click += (sender, e) => { prompt.Close(); };
+                prompt.button3.Click += (sender, e) => { prompt.Close(); };
+                prompt.ShowDialog();
+                if (prompt.name != String.Empty)
+                {
+                    button1.Content = prompt.name;
+                    button1.Style = (Style)Application.Current.Resources[prompt.style];
+                }
+                else
+                {
+                    button1.Content = String.Empty;
+                    button1.Style = (Style)Application.Current.Resources["Style2"];
+                }
+            }
+            else
+            {
+                //add item to sale
+            }
         }
     }
 }
