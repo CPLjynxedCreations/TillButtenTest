1e9764dd96ca84fa09f01ba66532fcb9c3593ec8
CPLjynxedCreations <CPLjynxedCreations@outlook.com>
2024-08-04 19:25:00 +1000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics.PerformanceData;
+using System.Printing;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ButtenTest
+{
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            button1.Style = (Style)Application.Current.Resources["Style2"];
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 prompt = new Window2();
+            prompt.Width = 1500;
+            prompt.Height = 500;
+            prompt.HorizontalAlignment = HorizontalAlignment.Center;
+            prompt.VerticalAlignment = VerticalAlignment.Center;
+            prompt.SizeToContent = SizeToContent.WidthAndHeight;
+            prompt.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            prompt.button2.Click += (sender, e) => { prompt.Close(); };
+            prompt.button3.Click += (sender, e) => { prompt.Close(); };
+            prompt.ShowDialog();
+            button1.Content = prompt.name;
+            button1.Style = (Style)Application.Current.Resources[prompt.style];
+        }
+    }
+}
---
+++ Window2.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ButtenTest
+{
+    /// <summary>
+    /// Interaction logic for Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        public string style;
+        public string name;
+
+        public Window2()
+        {
+            InitializeComponent();
+            MainWindow mainWindow = new MainWindow();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            style = "Style1";
+            name = boxName.Text;
+            //name = "Product";
+        }
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            style = "Style2";
+            name = String.Empty;
+            //name = "No Product";
+        }
+    }
+}
