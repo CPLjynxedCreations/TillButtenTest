994e418f41564b1a8db2f53a0696e249dd92645e
CPLjynxedCreations <CPLjynxedCreations@outlook.com>
2024-08-20 14:40:18 +1000

    removes and renames in accordance to index of UI stackpanel. just need to adjust amount adding to amount and price

--- Windows/TillWindow.xaml.cs
+++ Windows/TillWindow.xaml.cs
@@ -148,12 +148,12 @@ namespace ButtenTest
         {
 
             //added new button to stackpanel
-            count = stackThem2.Children.Count;
+            //int count = 0;//stackThem2.Children.Count;
             //var btnName = "btn" + count;
             var btnName = "btn";
             //var btnString;
-            var txtAmount = "txtAmount" + count;
-            var txtPrice = "txtPrice" + count;
+            var txtAmount = "txtAmount";// + count;
+            var txtPrice = "txtPrice";// + count;
 
             TextBlock block1 = new TextBlock();
             block1.Text = "1";
@@ -165,6 +165,7 @@ namespace ButtenTest
 
             Button btnClicked = (Button)sender;
             string btnString = Convert.ToString(btnClicked.Content);
+            Debug.WriteLine(btnString);
 
             ToggleButton btnAdd1 = new ToggleButton();
             btnAdd1.Content = btnString;
@@ -188,11 +189,14 @@ namespace ButtenTest
             int position = 0;
             string getAmountLabel;
             string getPriceLabel;
+            int setItem = 0;
 
             if (count > 0)
             {
                 for (int i = 0; i <= count; i++)
                 {
+                    Debug.WriteLine(i);
+
                     foreach (UIElement item in stackThem2.Children)
                     {
                         if (item.GetType() == typeof(ToggleButton))
@@ -201,6 +205,7 @@ namespace ButtenTest
                             if (tglBut.Content == btnString)
                             {
                                 position = stackThem2.Children.IndexOf(tglBut);
+                                Debug.WriteLine("position " + position);
                                 getPriceLabel = "txtPrice" + position;
                                 getAmountLabel = "txtAmount" + position;
                                 foreach (UIElement label in stackThem3.Children)
@@ -215,6 +220,7 @@ namespace ButtenTest
                                             intLinePrice = intLinePrice + Convert.ToInt32(newPrice);
                                             strLinePrice = Convert.ToString(intLinePrice);
                                             txtPriceLbl.Text = strLinePrice;
+                                            break;
                                         }
                                     }
                                 }
@@ -230,14 +236,17 @@ namespace ButtenTest
                                             intLineAmount = intLineAmount + 1;
                                             strLineAmount = Convert.ToString(intLineAmount);
                                             txtAmountLbl.Text = strLineAmount;
+                                            break;
                                         }
                                     }
                                 }
                                 notFound = false;
-                                return;
+                                break;
                             }
                             else
                             {
+                                setItem = i;
+                                Debug.WriteLine("set " + setItem);
                                 notFound = true;
                             }
                         }
@@ -245,17 +254,30 @@ namespace ButtenTest
                 }
                 if (notFound)
                 {
+                    btnAdd1.Name = btnName + setItem;
+                    block1.Name = txtAmount + setItem;
+                    block2.Name = txtPrice + setItem;
                     stackThem1.Children.Add(block1);
                     stackThem2.Children.Add(btnAdd1);
                     stackThem3.Children.Add(block2);
+                    count += 1;
+                    notFound = false;
+                    //return;
                 }
             }
             else
             {
+                block1.Name = txtAmount + 0;
+                btnAdd1.Name = btnName + 0;
+                block2.Name = txtPrice + 0;
                 stackThem1.Children.Add(block1);
                 stackThem2.Children.Add(btnAdd1);
-                btnAdd1.Name = btnName + 0;
                 stackThem3.Children.Add(block2);
+                count += 1;
+                Debug.WriteLine(btnAdd1.Name);
+                Debug.WriteLine(block1.Name);
+                Debug.WriteLine(block2.Name);
+                Debug.WriteLine("count " + count);
             }
             /*
             //set location of new window
@@ -269,26 +291,84 @@ namespace ButtenTest
         }
         private void btnRemove_Checked(object sender, RoutedEventArgs e)
         {
-            count = stackThem2.Children.Count;
+            //int count = stackThem2.Children.Count;
             ToggleButton toggleBut = (ToggleButton)sender;
             string name = toggleBut.Name;
+            bool deleted = false;
             for (int i = 0; i <= count; i++)
             {
                 //string name = "btn" + i;
                 foreach (UIElement item in stackThem2.Children)
                 {
+                    if (!deleted)
+                    {
+
+                        if (item.GetType() == typeof(ToggleButton))
+                        {
+                            //ToggleButton tglBut = (ToggleButton)item;
+                            if (toggleBut.Name == name)
+                            {
+                                stackThem2.Children.RemoveAt(i);
+                                stackThem1.Children.RemoveAt(i);
+                                stackThem3.Children.RemoveAt(i);
+                                toggleBut.IsChecked = false;
+                                count -= 1;
+                                deleted = true;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i <= count; i++)
+            {
+                var btnName = "btn";
+                var blockName1 = "txtAmount";
+                var blockName2 = "txtPrice";
+                //ToggleButton tglBut = new ToggleButton();
+                foreach (UIElement item in stackThem2.Children)
+                {
                     if (item.GetType() == typeof(ToggleButton))
                     {
-                        //ToggleButton tglBut = (ToggleButton)item;
-                        if (toggleBut.Name == name)
+                        ToggleButton tglBut = (ToggleButton)item;
+                        if (stackThem2.Children.IndexOf(tglBut) == i)
                         {
-                            stackThem2.Children.RemoveAt(i);
-                            stackThem1.Children.RemoveAt(i);
-                            stackThem3.Children.RemoveAt(i);
-                            toggleBut.IsChecked = false;
-                            count -= 1;
-                            return;
+                            tglBut.Name = btnName + i; 
+                        Debug.WriteLine("index " + stackThem2.Children.IndexOf(tglBut));
+                        Debug.WriteLine("new name " + tglBut.Name);
+
                         }
+                        //if ()
+                    }
+                }
+                foreach (UIElement item in stackThem1.Children)
+                {
+                    if (item.GetType() == typeof(TextBlock))
+                    {
+                        TextBlock txtblock = (TextBlock)item;
+                        if (stackThem1.Children.IndexOf(txtblock) == i)
+                        {
+                            txtblock.Name = blockName1 + i;
+                            Debug.WriteLine("index " + stackThem1.Children.IndexOf(txtblock));
+                            Debug.WriteLine("new name " + txtblock.Name);
+
+                        }
+                        //if ()
+                    }
+                }
+                foreach (UIElement item in stackThem3.Children)
+                {
+                    if (item.GetType() == typeof(TextBlock))
+                    {
+                        TextBlock txtblock = (TextBlock)item;
+                        if (stackThem3.Children.IndexOf(txtblock) == i)
+                        {
+                            txtblock.Name = blockName2 + i;
+                            Debug.WriteLine("index " + stackThem1.Children.IndexOf(txtblock));
+                            Debug.WriteLine("new name " + txtblock.Name);
+
+                        }
+                        //if ()
                     }
                 }
             }
@@ -297,7 +377,7 @@ namespace ButtenTest
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             //checks for text in textBlock then adds all together
-            int count = stackThem2.Children.Count;
+            //int count = stackThem2.Children.Count;
             string strAmount;
             int amount;
 
